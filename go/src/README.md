a21e479ce371a988f52d1db56acc81adb6248e55
Rob Cameron <rcameron@juniper.net>
2014-03-20 10:15:00 -0700

    adding port option for webserver

---
+++ go/src/README.md
@@ -0,0 +1,10 @@
+This is a simple webserver to serve kickstart files. It listens on port 8080 and all IPs/Interfaces/Protocols.
+
+Operation:
+go run simpleHTTP.go -d /path/to/serve
+
+Build:
+go build simpleHTTP.go
+
+Bonus:
+Supports win/mac/linux/freebsd!
--- go/src/simpleHttp.go
+++ go/src/simpleHttp.go
@@ -7,6 +7,7 @@ import (
 )
 
 var dirFlag = flag.String("directory","","Specify directory to serve")
+var portFlag = flag.String("port","","Specify port to listen on")
 
 func Log(handler http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -16,7 +17,8 @@ func Log(handler http.Handler) http.Handler {
 }
 
 func init() {
-    flag.StringVar(dirFlag,"d","","Specify directory to serve")
+    flag.StringVar(dirFlag,"d","","Specify directory to serve (Default: .)")
+    flag.StringVar(portFlag,"p","","Specify port to listen on (Default: 8080)")
 }
 
 func main() {
